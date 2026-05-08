072b4f1c3bffced30c3d3f2c67353ae2d4239944
Juan Muñoz <cl.jmunoz@gmail.com>
2026-05-08 15:14:27 -0400

    chore: update Next.js build artifacts and trace logs

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -180,7 +180,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9c54ea44dfce91c9"; }
+/******/ 		__webpack_require__.h = function() { return "1e0c98352620448e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
