f8d404481806add2c9ada7a65f99cc0ca2f1f722
Juan Muñoz <cl.jmunoz@gmail.com>
2026-05-17 18:57:43 -0400

    chore: add environment configuration for Supabase in production and development

---
+++ .open-next/cloudflare/next-env.mjs
@@ -0,0 +1,3 @@
+export const production = {"NEXT_PUBLIC_SUPABASE_URL":"https://xsqyftoblnsjkavhldcm.supabase.co","NEXT_PUBLIC_SUPABASE_ANON_KEY":"sb_publishable_yIPrIzSpveyYkJpdbFsdsg_ofYGRzag","NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY":"sb_publishable_yIPrIzSpveyYkJpdbFsdsg_ofYGRzag"};
+export const development = {"NEXT_PUBLIC_SUPABASE_URL":"https://xsqyftoblnsjkavhldcm.supabase.co","NEXT_PUBLIC_SUPABASE_ANON_KEY":"sb_publishable_yIPrIzSpveyYkJpdbFsdsg_ofYGRzag","NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY":"sb_publishable_yIPrIzSpveyYkJpdbFsdsg_ofYGRzag"};
+export const test = {};
