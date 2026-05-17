cc4cf36e2fe7415abe34e81928be17d251fdf8b6
Juan Muñoz <cl.jmunoz@gmail.com>
2026-05-16 23:36:40 -0400

    chore: update Next.js configuration and add new wrangler.toml file

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
   images: {
-    unoptimized: true, // Requerido con output: 'export'
     remotePatterns: [
       {
         protocol: 'https',
